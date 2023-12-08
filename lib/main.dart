ae24cb10e141e0b60b20555a450183b304fdf3eb
Kunal Gharate <kunalgharate@gmail.com>
2023-12-08 19:19:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:oauth2connection/Service.dart';
+import 'package:path/path.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  File? selectedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Upload Example"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.25,
+                child: FutureBuilder(
+                  future: _getImage(context),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                        return Text('Please wait');
+                      case ConnectionState.waiting:
+                        return Center(child: CircularProgressIndicator());
+                      default:
+                        if (snapshot.hasError)
+                          return Text('Error: ${snapshot.error}');
+                        else {
+                          return selectedImage != null
+                              ? Image.file(selectedImage!)
+                              : Center(
+                            child: Text("Please Get the Image"),
+                          );
+                        }
+                    }
+                  },
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade500)),
+              ),
+            ],
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(
+            onPressed: (){
+              Service service=Service();
+              service.submitSubscription(file:selectedImage!,
+                  filename:basename(selectedImage!.path),token:"4f7d97727db95c9709e7198a6898a24d6eef6a58");
+            },
+            child: Text("Upload"),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+  //get image from camera
+  Future getImage() async {
+    ImagePicker imagePicker = new ImagePicker();
+    final XFile? image = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+     selectedImage = File(image!.path);
+    });
+    //return image;
+  }
+
+  //resize the image
+  Future<void> _getImage(BuildContext context) async {
+    if (selectedImage != null) {
+      var imageFile = selectedImage;
+      /*var image = imageLib.decodeImage(imageFile.readAsBytesSync());
+      fileName = basename(imageFile.path);
+      image = imageLib.copyResize(image,
+          width: (MediaQuery.of(context).size.width * 0.8).toInt(),
+          height: (MediaQuery.of(context).size.height * 0.7).toInt());
+      _image = image;*/
+    }
+  }
+}
